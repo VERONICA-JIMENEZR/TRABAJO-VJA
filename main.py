71b6a34e501ba4de7aca8077a3f6a3fa1f480488
Verónica Jiménez Ramírez <veronica.jimenez1@udea.edu.co>
2024-06-10 13:43:51 -0500

    Supuestamente solucionado

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from fastapi import FastAPI, HTTPException, Depends, Security
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from fastapi.responses import JSONResponse
 import pandas as pd
 import matplotlib.pyplot as plt
 from io import BytesIO
 
+app = FastAPI()
 
-app: FastAPI = FastAPI()
-
-security: HTTPBasic = HTTPBasic()
+security = HTTPBasic()
 
 # Credenciales de acceso
-fake_users_db: dict = {
+fake_users_db = {
     "Vero": "1234"  # Usuario: Vero, Contraseña: 1234
 }
 
-clientes_df: pd.DataFrame = pd.read_csv("clientes_segmentados.csv", index_col="ID cliente")
-productos_mas_vendidos_df: pd.DataFrame = pd.read_csv("productos_mas_vendidos.csv")
-productos_menos_vendidos_df: pd.DataFrame = pd.read_csv("productos_menos_vendidos.csv")
-resultados_cohorte_df: pd.DataFrame = pd.read_csv("resultados_cohorte.csv")
+# Función para verificar las credenciales
+def verificar_credenciales(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    password = credentials.password
+    if username not in fake_users_db or fake_users_db[username] != password:
+        raise HTTPException(status_code=401, detail="Nombre de usuario o contraseña incorrectos")
+    return True
+
+clientes_df = pd.read_csv("clientes_segmentados.csv", index_col="ID cliente")
+productos_mas_vendidos_df = pd.read_csv("productos_mas_vendidos.csv")
+productos_menos_vendidos_df = pd.read_csv("productos_menos_vendidos.csv")
+resultados_cohorte_df = pd.read_csv("resultados_cohorte.csv")
 
 @app.get("/")
 async def obtener_mensaje() -> dict:
@@ -28,34 +34,18 @@ async def obtener_mensaje() -> dict:
     """
     return {"message": "¡Bienvenido a la API de Eureka! Esta API proporciona información sobre productos más y menos vendidos, así como el producto más consumido por mes."}
 
-
-@app.get("/token")
-async def login(credentials: HTTPBasicCredentials = Depends(security)) -> dict:
-    """
-    Obtener token de autenticación.
-    """
-    correct_username: str = fake_users_db.get(credentials.username)
-    correct_password: str = fake_users_db.get(credentials.password)
-    if not (correct_username and correct_password and
-            correct_username == credentials.username and
-            correct_password == credentials.password):
-        return JSONResponse(status_code=401, content={"message": "Nombre de usuario o contraseña incorrectos"})
-    return {"token": "token_de_eureka"}
-
-
-
 @app.get("/clientes/{segmento}")
-async def obtener_clientes(segmento: str) -> dict:
+async def obtener_clientes(segmento: str, authenticated: bool = Depends(verificar_credenciales)) -> dict:
     """
     Obtener clientes por segmento.
     """
     if segmento.lower() not in ["oro", "plata", "bronce"]:
         raise HTTPException(status_code=404, detail="Segmento no válido")
 
-    clientes_segmento: pd.DataFrame = clientes_df[clientes_df["Cliente"] == segmento.capitalize()]
-    clientes: list = clientes_segmento["Nombre"].tolist()
-    cantidad: int = len(clientes)
-    porcentaje: float = (cantidad / len(clientes_df)) * 100
+    clientes_segmento = clientes_df[clientes_df["Cliente"] == segmento.capitalize()]
+    clientes = clientes_segmento["Nombre"].tolist()
+    cantidad = len(clientes)
+    porcentaje = (cantidad / len(clientes_df)) * 100
 
     return {
         "segmento": segmento.capitalize(),
@@ -66,14 +56,14 @@ async def obtener_clientes(segmento: str) -> dict:
 
 # CRUD - Create
 @app.post("/clientes")
-async def agregar_cliente(nombre: str, cliente: str) -> dict:
+async def agregar_cliente(nombre: str, cliente: str, authenticated: bool = Depends(verificar_credenciales)) -> dict:
     """
     Agregar un nuevo cliente.
     """
     if cliente.lower() not in ["oro", "plata", "bronce"]:
         raise HTTPException(status_code=404, detail="Segmento no válido")
 
-    nuevos_datos: pd.DataFrame = pd.DataFrame({"Nombre": [nombre], "Cliente": [cliente.capitalize()]})
+    nuevos_datos = pd.DataFrame({"Nombre": [nombre], "Cliente": [cliente.capitalize()]})
     global clientes_df
     clientes_df = pd.concat([clientes_df, nuevos_datos])
     clientes_df.to_csv("clientes_segmentados.csv")
@@ -81,16 +71,15 @@ async def agregar_cliente(nombre: str, cliente: str) -> dict:
 
 # CRUD - Read
 @app.get("/clientes")
-async def obtener_todos_los_clientes():
+async def obtener_todos_los_clientes(authenticated: bool = Depends(verificar_credenciales)):
     """
     Obtener todos los clientes segmentados.
     """
     return clientes_df.to_dict(orient="index")
 
-
 # CRUD - Update
 @app.put("/clientes/{id_cliente}")
-async def actualizar_cliente(id_cliente: str, nombre: str, cliente: str):
+async def actualizar_cliente(id_cliente: str, nombre: str, cliente: str, authenticated: bool = Depends(verificar_credenciales)):
     """
     Actualizar información de un cliente.
     """
@@ -103,18 +92,11 @@ async def actualizar_cliente(id_cliente: str, nombre: str, cliente: str):
     clientes_df.to_csv("clientes_segmentados.csv")
     return {"message": "Cliente actualizado exitosamente"}
 
-
 # CRUD - Delete
 @app.delete("/clientes/{id_cliente}")
-async def eliminar_cliente(id_cliente: str) -> dict:
+async def eliminar_cliente(id_cliente: str, authenticated: bool = Depends(verificar_credenciales)) -> dict:
     """
     Eliminar un cliente.
-
-    Parameters:
-        id_cliente (str): Identificador del cliente a eliminar.
-
-    Returns:
-        dict: Mensaje de éxito en la eliminación del cliente.
     """
     if id_cliente not in clientes_df.index:
         raise HTTPException(status_code=404, detail="Cliente no encontrado")
@@ -123,9 +105,8 @@ async def eliminar_cliente(id_cliente: str) -> dict:
     clientes_df.to_csv("clientes_segmentados.csv")
     return {"message": "Cliente eliminado exitosamente"}
 
-
 @app.get("/grafica", response_class=StreamingResponse)
-async def obtener_grafica() -> StreamingResponse:
+async def obtener_grafica(authenticated: bool = Depends(verificar_credenciales)) -> StreamingResponse:
     """
     Obtener gráfica de la proporción de clientes por segmento.
     """
@@ -151,19 +132,10 @@ async def obtener_grafica() -> StreamingResponse:
     
     return StreamingResponse(buf, media_type="image/png")
 
-
-
 @app.get("/producto-más-consumido")
-async def obtener_producto_mas_consumido(año: int, mes: int) -> dict:
+async def obtener_producto_mas_consumido(año: int, mes: int, authenticated: bool = Depends(verificar_credenciales)) -> dict:
     """
     Obtener el producto más consumido del mes.
-
-    Parameters:
-        año (int): Año de la consulta.
-        mes (int): Mes de la consulta.
-
-    Returns:
-        dict: Información sobre el producto más consumido.
     """
     try:
         producto_mas_consumido = resultados_cohorte_df.loc[(resultados_cohorte_df["Año"] == año) &
@@ -174,29 +146,19 @@ async def obtener_producto_mas_consumido(año: int, mes: int) -> dict:
 
     return {"año": año, "mes": mes, "producto_mas_consumido": producto_mas_consumido}
 
-
 @app.get("/productos-más-vendidos")
-async def obtener_productos_mas_vendidos() -> dict:
+async def obtener_productos_mas_vendidos(authenticated: bool = Depends(verificar_credenciales)) -> dict:
     """
     Obtener lista de los 15 productos más vendidos.
-
-    Returns:
-        dict: Lista de los 15 productos más vendidos.
     """
-    # Seleccionar los primeros 15 productos más vendidos
     productos_mas_vendidos = productos_mas_vendidos_df.head(15)["Nombre producto"].tolist()
     return {"productos_mas_vendidos": productos_mas_vendidos}
 
-# Función para obtener los 15 productos menos vendidos
 @app.get("/productos-menos-vendidos")
-async def obtener_productos_menos_vendidos() -> dict:
+async def obtener_productos_menos_vendidos(authenticated: bool = Depends(verificar_credenciales)) -> dict:
     """
     Obtener lista de los 15 productos menos vendidos.
-
-    Returns:
-        dict: Lista de los 15 productos menos vendidos.
     """
-    # Seleccionar los últimos 15 productos menos vendidos
     productos_menos_vendidos = productos_menos_vendidos_df.tail(15)["Nombre producto"].tolist()
     return {"productos_menos_vendidos": productos_menos_vendidos}
 
